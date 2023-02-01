7ff1761d3a4b1923c7bdb3e493715165cbc85ebc
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-01-31 20:52:36 -0500

    Moved HomeScreen and ProductScreen to Express Server

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -53,11 +53,11 @@ const Rating = ({value, text, color}) => {
 Rating.defaultProps = {
     color: '#f8e825',
 }
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string,
+// Rating.propTypes = {
+//     value: PropTypes.number.isRequired,
+//     text: PropTypes.string.isRequired,
+//     color: PropTypes.string,
 
-}
+// }
 
 export default Rating
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { Link} from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import { useParams } from 'react-router-dom'
 
+const ProductScreen = () => {
+    const [product, setProduct] = useState({})
+    const { id } = useParams()
+  
+    useEffect(() => {
+      const fetchProduct = async () => {
+        const { data } = await axios.get(`/api/products/${encodeURIComponent(id)}`)
+        setProduct(data)
+      }
+  
+      fetchProduct()
+    }, [ id ])
 
-const ProductScreen = ({match}) => {
-    const {id} = useParams();
-    const product = products.find((p) => p._id === (id));
-   
   return <>
     <Link className='btn btn-dark my-3' to='/'>
     Go Back
